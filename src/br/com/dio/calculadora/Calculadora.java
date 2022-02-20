f659b3d2c178c9da157feb8b4fb4f96dd08933b8
Juliana Rios <julianariosone@gmail.com>
2022-02-20 19:45:10 -0300

    update calculadora

---
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,36 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner scan =  new Scanner(System.in);
+        int a, b;
+
+        System.out.println("Digite o primeiro valor: ");
+        a = scan.nextByte();
+        System.out.println("Digite o segundo valor: ");
+        b = scan.nextInt();
+
+        int somar = somar(a, b);
+        int subtracao = subtracao(a, b);
+        int multiplicacao = multiplicacao(a, b);
+        int divisao = divisao(a, b);
+
+        System.out.println("A soma é: " + somar);
+        System.out.println("A subtração é: " + subtracao);
+        System.out.println("A multiplicação é: " + multiplicacao);
+        System.out.println("A divisão é: " + divisao);
+    }
+
+    public static int somar(int a, int b){
+        return a + b;
+
+    }public static int subtracao(int a, int b){
+        return a - b;
+    }public static int multiplicacao(int a, int b){
+        return a * b;
+    }public static int divisao(int a, int b){
+        return a / b;
+    }
+}
